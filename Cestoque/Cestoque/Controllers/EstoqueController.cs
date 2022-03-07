d20807e516422646ea63d9b1a23537bf2401bf27
smichelle-dev <dev.smichelle@gmail.com>
2022-03-07 16:36:14 -0300

    criação da controller estoque e views

---
+++ Cestoque/Cestoque/Controllers/EstoqueController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cestoque.Controllers
+{
+    public class EstoqueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+
+        public IActionResult EditarProduto()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarProduto()
+        {
+            return View();
+        }
+
+    }
+}
