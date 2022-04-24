05a95983250d0ca1d58898e88fc37b2c821ab175
smichelle-dev <dev.smichelle@gmail.com>
2022-04-24 20:26:30 -0300

    criação controller Cesta e Logistica

---
+++ Cestoque/Cestoque/Controllers/LogisticaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cestoque.Controllers
+{
+    public class LogisticaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+    }
+
+}
